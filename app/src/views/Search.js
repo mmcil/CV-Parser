42d8471088424d1e139f64f4ed240c4f60fdfc51
mcil <mcil@havelsan.com.tr>
2021-06-02 13:51:27 +0300

    Search screen is updated.

--- app/src/views/Search.js
+++ app/src/views/Search.js
@@ -1,12 +1,173 @@
-import React, {Component} from "react";
-import SearchBar from './SearchBar';
+import validate from 'validate';
+import PropTypes from 'prop-types';
+import {cloneDeep, get, pickBy} from 'lodash';
+import {FormattedMessage} from 'react-intl';
+import SaveIcon from '@material-ui/icons/Save';
+import EditIcon from '@material-ui/icons/Edit';
+import {makeStyles} from '@material-ui/styles';
+import RemoveIcon from '@material-ui/icons/Delete';
+import React, {useContext, useEffect, useRef, useState} from 'react';
 
-class Search extends Component {
-    render() {
-        return (
-            <SearchBar />
-        );
-    }
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    CircularProgress,
+    colors,
+    Divider,
+    Fab,
+    Grid,
+    TextField,
+    Typography,
+} from '@material-ui/core';
+
+const initialValues = {
+    id: '',
+    name: '',
+    email: '',
+    mobile_number: '',
+    college_name: '',
+    degree: '',
+    skills: '',
+    company_names: '',
+    experience: '',
+    total_experience: '',
+};
+
+const initialFormValidate = {
+    isValid: false,
+    touched: {},
+    errors: {},
+};
+
+const Search = () => {
+    const [formState, setFormState] = useState(initialValues);
+    const [formValidate, setFormValidate] = useState(initialFormValidate);
+
+    const handleFieldChange = (name) => (e) => {
+        e.persist();
+
+        setFormState((state) => ({
+            ...state,
+            [name]: e.target.type === 'checkbox' ? e.target.checked : e.target.value,
+        }));
+
+        setFormValidate((state) => ({
+            ...state,
+            touched: {
+                ...state.touched,
+                [name]: true,
+            },
+        }));
+    };
+
+    const hasError = (field) =>
+        !!(formValidate.touched[field] && formValidate.errors[field]);
+
+    return (
+        <div>
+            <Grid className={'right'} container spacing={2}>
+
+                <Grid item md={6} xs={12}>
+                    <TextField
+                        fullWidth
+                        label={'Name'}
+                        onChange={handleFieldChange('name')}
+                        value={formState.name}
+                        variant={'outlined'}
+                        error={hasError('name')}
+                    />
+                </Grid>
+
+                <Grid item md={6} xs={12}>
+                    <TextField
+                        fullWidth
+                        label={'E-mail'}
+                        onChange={handleFieldChange('email')}
+                        value={formState.email}
+                        variant={'outlined'}
+                        error={hasError('email')}
+                    />
+                </Grid>
+
+                <Grid item md={6} xs={12}>
+                    <TextField
+                        fullWidth
+                        label={'Mobile Number'}
+                        onChange={handleFieldChange('mobile_number')}
+                        value={formState.mobile_number}
+                        variant={'outlined'}
+                        error={hasError('mobile_number')}
+                    />
+                </Grid>
+
+                <Grid item md={6} xs={12}>
+                    <TextField
+                        fullWidth
+                        label={'College'}
+                        onChange={handleFieldChange('college_name')}
+                        value={formState.college_name}
+                        variant={'outlined'}
+                        error={hasError('college_name')}
+                    />
+                </Grid>
+
+                <Grid item md={6} xs={12}>
+                    <TextField
+                        fullWidth
+                        label={'Degree'}
+                        value={formState.degree}
+                        variant={'outlined'}
+                        error={hasError('degree')}
+                    />
+                </Grid>
+
+                <Grid item md={6} xs={12}>
+                    <TextField
+                        fullWidth
+                        label={'Companies'}
+                        value={formState.company_names}
+                        variant={'outlined'}
+                        error={hasError('company_names')}
+                    />
+                </Grid>
+
+                <Grid item md={6} xs={12}>
+                    <TextField
+                        fullWidth
+                        label={'Experience'}
+                        value={formState.experience}
+                        variant={'outlined'}
+                        error={hasError('experience')}
+                    />
+                </Grid>
+
+                <Grid item md={6} xs={12}>
+                    <TextField
+                        fullWidth
+                        label={'Total Experience'}
+                        value={formState.total_experience}
+                        variant={'outlined'}
+                        error={hasError('total_experience')}
+                    />
+                </Grid>
+
+                <Grid item md={12} xs={12}>
+                    <TextField
+                        fullWidth
+                        label={'Skills'}
+                        value={formState.skills}
+                        variant={'outlined'}
+                        error={hasError('skills')}
+                    />
+                </Grid>
+
+            </Grid>
+        </div>
+
+    );
+
 }
 
 export default Search;
